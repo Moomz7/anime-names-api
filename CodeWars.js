09e019ac8e15b8a1ad0ef230510a101767d5833c
Moomz7 <Moumie.dibinga@gmail.com>
2025-01-10 22:41:02 -0500

    2nd CodeWars completed!

--- CodeWars.js
+++ CodeWars.js
@@ -2,6 +2,16 @@
 
 // Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
 
-function move (position, roll) {
-    return position + roll*2
-  }
+// function move (position, roll) {
+//     return position + roll*2
+//   }
+
+// Nathan loves cycling.
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+// function litres(time) {
+//   return Math.floor(time * 0.5)
+// }
