add769c6fdd36520758bcf7f4edd3085e81de13e
Moomz7 <Moumie.dibinga@gmail.com>
2025-01-11 23:42:01 -0500

    My 3rd CodeWars!

--- CodeWars.js
+++ CodeWars.js
@@ -15,3 +15,11 @@
 // function litres(time) {
 //   return Math.floor(time * 0.5)
 // }
+
+//Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+// function solution(nums){
+//     return (nums || []).sort(function(a, b){
+//       return a - b
+//     });
+//   }
